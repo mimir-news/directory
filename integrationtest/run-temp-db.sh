7cfb8f17ae989fdd9ba3a50392db0930a043e9bc
Simon Lindgren <simon.g.lindgren@gmail.com>
2018-12-27 13:05:32 +0100

    Added script to start a temporary db

---
+++ integrationtest/run-temp-db.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+TEST_ID=$(openssl rand -hex 5)
+GIT_COMMIT=$(git rev-parse HEAD)
+SHORT_COMMIT="${GIT_COMMIT:0:7}"
+
+# Service metadata
+APPV_FILE="../appv.json"
+SVC_NAME=$(jq '.name' -r $APPV_FILE)
+SVC_VERSION=$(jq '.version' -r $APPV_FILE)
+SVC_REGISTRY=$(jq '.registry' -r $APPV_FILE)
+SVC_IMAGE="$SVC_REGISTRY/$SVC_NAME:$SVC_VERSION"
+SVC_CONTAINER_NAME="$SVC_NAME-$SVC_VERSION-$SHORT_COMMIT-$TEST_ID"
+
+# Database metadata
+DB_IMAGE='postgres:11.1-alpine'
+DB_CONTAINER_NAME="directory-db-$TEST_ID"
+
+# Database setup
+echo "Starting database: $DB_CONTAINER_NAME"
+docker run -d --rm --name $DB_CONTAINER_NAME -p 5432:5432 \
+   -e POSTGRES_PASSWORD=password $DB_IMAGE
+
+echo "Sleeping for 2 seconds to make database ready"
+sleep 2
+
+echo 'Setup up database and user'
+docker exec -i $DB_CONTAINER_NAME psql -U postgres < conf/db_setup.sql
+
+echo 'Database ready'
